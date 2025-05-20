2efa727346b9eb741fd9f308a79acb6d03fd6aad
cristianlarad <die.hts1@gmail.com>
2025-05-20 17:26:16 -0400

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={`${fontSans.className}  antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
