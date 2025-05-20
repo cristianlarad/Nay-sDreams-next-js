4b8269c4c2a185a9f0860063e0268638f4fc9920
cristianlarad <die.hts1@gmail.com>
2025-05-20 16:17:24 -0400

    fix team suwtcher

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -6,8 +6,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/dropdown-menu";
